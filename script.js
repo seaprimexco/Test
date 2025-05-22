755b2bae49576ce4a4ce991f40570a52b95fce00
seaprimexco <tchc.seaprimexco@gmail.com>
2025-05-22 13:43:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// Thay URL này bằng Google Apps Script Web App URL của Đại ca
+const scriptURL = 'https://script.google.com/macros/s/AKfycbysOvcDSUBDu7Mtp1NTyKxaWuIPuKd9HwHO-TCycq_hmDwpt0YtEKD1GX7lt2pETbtNiQ/exec';
+
+const form = document.getElementById('surveyForm');
+const status = document.getElementById('status');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = {
+    hoten: form.hoten.value,
+    chi_phi: form.chi_phi.value
+  };
+
+  status.innerText = 'Đang gửi...';
+
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then(response => {
+    status.innerText = 'Gửi thành công!';
+    form.reset();
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    status.innerText = 'Lỗi gửi dữ liệu!';
+  });
+});
